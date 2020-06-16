da0d967713fff15d8ae69d3a9bd1e069284aab2d
SclRlLiX <georg.m985@gmail.com>
2020-06-16 12:42:30 +0200

    added output for debugging

--- mouse.py
+++ mouse.py
@@ -1,9 +1,8 @@
 import pyautogui
 import time 
 
-
-
 starttime=time.time()
 while True: 
     pyautogui.moveRel(10,10, duration=2)
-    time.sleep(300.0 - ((time.time() - starttime) % 300.0))
+    print("moving now")
+    time.sleep(600.0 - ((time.time() - starttime) % 600.0))
