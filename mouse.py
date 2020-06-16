165eb882a3cf8476810aed85ef26e115f955b6a4
SclRlLiX <georg.m985@gmail.com>
2020-06-16 13:21:12 +0200

    added vbs script

--- mouse.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import time 
-
-starttime=time.time()
-while True: 
-    pyautogui.moveRel(10,10, duration=2)
-    print("moving now")
-    time.sleep(600.0 - ((time.time() - starttime) % 600.0))
